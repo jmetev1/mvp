5f6c3aea8ff72706cacee64a050f5da3b91eff8a
Jmetev1 <jmetevier@gmail.com>
2017-07-28 13:50:07 -0500

    server changes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 
 var app = express();
-const env = Object.assign({}, process.env, {PORT: 5000});
 
 app.set('port', (process.env.PORT || 5000));
 
 // var port = 8080;
-// app.listen(port)
+app.listen(app.get('port'));
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
 
 app.use(express.static(__dirname +'/client'))
