d284d942ede2f67f8c51da91ef33af55bd1f52b5
Jmetev1 <jmetevier@gmail.com>
2017-07-28 13:36:31 -0500

    edit packjson

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 
 var app = express();
-var port = 8080;
-// app.listen(port,'http://localhost', () => {
-//   console.log('Server now listening on port ' + port);
-// });
-app.listen(port)
+const env = Object.assign({}, process.env, {PORT: 5000});
+
+app.set('port', (process.env.PORT || 5000));
+
+// var port = 8080;
+// app.listen(port)
 
 app.use(express.static(__dirname +'/client'))
