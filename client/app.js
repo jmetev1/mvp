e2b5e6a384bc1db23ce6838fa329f4455ae87eef
Jmetev1 <jmetevier@gmail.com>
2017-07-29 10:21:14 -0500

    click to add marker and lang long data

--- client/app.js
+++ client/app.js
@@ -12,16 +12,28 @@ var app = angular.module('app', [])
   })
   .component('showMap', {
     templateUrl: './indexTemplate.html',
-    controller: function MapControl(sendService, $scope) {
-      $scope.coord = '100';
-      $scope.submit = function(email, password) {
-
-        console.log(sendService, email, $scope.password)
-      }      
-      this.wolf = 'coyote100'
-      this.coord = 100
-
-    }
+    controller: function MapControl(sendService) {
+      this.submit = function(email, password) {
+        console.log('coyo', email)
+        console.log('in init map')
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+        this.coord = uluru.lat;
+          google.maps.event.addListener(map, 'click', function(event) {
+            marker = new google.maps.Marker({
+              position: event.latLng,
+              map: map
+            });
+            console.log(marker.position)
+        })
+        this.wolf = 'coyote100';
+      };
+    },
   });
-
-  
